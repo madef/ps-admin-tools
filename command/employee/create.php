5d1a15cd0c21449d298c2b9feeefc5ae4b22aa47
Maxence de Flotte <maxence@deflotte.fr>
2017-05-28 09:42:03 +0200

    [*] Add missing licence header

--- command/employee/create.php
+++ command/employee/create.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * 2013-2017 MADEF IT
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@madef.fr so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    MADEF IT <contact@madef.fr>
+ *  @copyright 2013-2017 MADEF IT
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
 
 class AT_Employee_Create extends AT_Command_Abstract
 {
